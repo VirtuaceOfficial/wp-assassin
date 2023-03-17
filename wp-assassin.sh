fb1dd15b3e6ec9463bd6ce956239868889183382
Zak Probst <58903482+VirtuaceOfficial@users.noreply.github.com>
2023-03-17 07:44:07 -0600

    Add files via upload
    >Significant feature additions, minor bugfixes, and title added. 
    >Moved the log naming selection to beginning of script
    >More to come soon

--- wp-assassin.sh
+++ wp-assassin.sh
@@ -8,7 +8,11 @@ clear
 title="WordPress Assassin | By Virtuace Security"
 echo -e "$title\n"
 read -p "Enter Target Domain (example.com): " target
-option=("WordPress Port Scan" "WordPress Vulnerability Scan" "Web Server Header Exploitation" "Web Server Vulnerability Scan" "Web Server Directory Attacks" "Quit")
+read -p "Enter name for logfile: " logname
+clear
+echo -e "$title\n"
+
+option=("WordPress Port Scan" "WordPress Vulnerability Scan" "Web Server Header Exploitation" "Web Server Vulnerability Scan" "Web Server Exploitation" "Quit")
 
 select opt in "${option[@]}"
 do
@@ -19,8 +23,8 @@ do
             echo -e "$title\n"
             nslookup $target| grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $NF}' | sed '1,2d' > iptarget.txt
             iptarget=$(cat iptarget.txt)
-            read -p "Enter name for logfile: " logname
-            echo -e "\nTarget Set, How should I scan?\n"
+            #read -p "Enter name for logfile: " logname
+            echo -e "\nTarget Selected $target, How should I scan?\n"
                 
                 scanoptions=("Full TCP Port Scan" "Common TCP Ports" "SSH, FTP, and RDP Ports Only")
 
@@ -29,29 +33,31 @@ do
                     case $opts in
                         "Full TCP Port Scan")
                         clear
+                        echo -e "$title\n"
                         outputlog=("logs/full_tcp_scan-$target-$logname.txt")
                         echo -e "Full TCP Port Scan Selected.\n Output file will be saved to $outputlog"
                         echo -e "\nSelected Target: $target\nServer IP: $iptarget\nPlease be patient, this may take several minutes. \n"
                         sudo nmap -sS -A -p- -T4 -v $iptarget -oN $outputlog
                         clear
+                        echo -e "$title\n"
                         echo -e "Scan Completed.\nOpen Ports Found on the server:\n"
-                        cat $outputlog | grep "/tcp open"
-                        
+                        cat $outputlog | grep "/tcp open"                       
                         ;;
 
                         "Common TCP Ports")
                         clear
+                        echo -e "$title\n"
                         outputlog=("logs/common_ports-$target-$logname.txt")
                         echo -e "Full TCP Port Scan Selected.\n Output file will be saved to $outputlog"
                         echo -e "\nScanning 13 TCP Ports on $target, this may take a moment \n"
                         sudo nmap -sS -p T:21,22,23,25,53,80,443,554,3306,161,162,445,5432,179 -T4 -v $iptarget
                         echo -e "Scan Completed.\n"
-                        cat $outputlog | grep "/tcp open"
-                        #sh ./return.sh
+                        cat $outputlog | grep "/tcp open"        
                         ;;
 
                         "SSH, FTP, and RDP Ports Only")
                         clear
+                        echo -e "$title\n"
                         outputlog=("logs/ssh_tcp_rdp-$target-$logname.txt")
                         echo -e "Full TCP Port Scan Selected.\n Output file will be saved to $outputlog"
                         echo -e "\nScanning 13 TCP Ports on $target, this may take a moment \n"
@@ -64,14 +70,14 @@ do
                     *) echo -e "Invalid Option $REPLY";;
                     esac
                 done
-
     ;;
+    
         "WordPress Vulnerability Scan")
             clear
+            echo -e "$title\n"
             nslookup $target| grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $NF}' | sed '1,2d' > iptarget.txt
             iptarget=$(cat iptarget.txt)
-            read -p "Enter name for logfile: " logname
-            echo -e "\nTarget Set, How should I scan?\n"
+            echo -e "\nTarget Selected $target, How should I scan?\n"
 
             vulnscanopts=("Firewall Detection Scan" "Web Server Vulnerability Scan" "Plugin Vulnerability Analysis" "Client Side Attack Vector Vuln" "Main Menu")
             
@@ -80,30 +86,44 @@ do
                 case $opts2 in
                     "Firewall Detection Scan")
                     clear
+                    echo -e "$title\n"
                     wafw00f $target
                     echo -e "\nPress 'Enter' to return to vulnerability menu.\n"
-                    sleep 1
-                    echo -e "Automatically returning in 5 seconds...\n"
-                    sleep 1
-                    echo -e "Returning in 4" 
-                    sleep 1
-                    echo -e "Returning in 3" 
-                    sleep 1
-                    echo -e "Returning in 2" 
-                    sleep 1
-                    echo -e "Returning in 1" 
-                    sleep 1
-                    #sh ./return.sh
+                ;;  
+                    "Web Server Vulnerability Scan")
+                    clear
+                    echo -e "$title\n"
+                    echo -e "\t\t\t-- WARNING --\nIT IS UNADVISED TO RUN THIS TOOL WITHOUT PROXYCHAINS ENABLED \n"
                     
-
+                    nikto -host $target
+                ;;
+                    "Plugin Vulnerability Analysis")
+                    clear
+                    echo -e "$title\n"
+                    read -p "Enter Plugin or Service Name: " plugin_name
+                    read -p "Enter Plugin Port: " plugin_port
+                    echo -e "\n Plugin/Service Selected: $plugin_name"
+                    echo -e "\n Plugin Port: $plugin_port\n"
+                ;;
+                    "Client Side Attack Vector Vuln")
+                    clear
+                    echo -e "$title\n"
+                    echo -e "Under Construction. Press Enter to go back."
+                ;;
+                    "Main Menu")
+                    clear
+                    sh ./wp-assassin.sh
                 ;;
                 *)    
                 esac
             done
     ;;
-        "Web Server Header Exploitation")
+        "Web Server Exploitation")
             clear
-
+            echo -e "$title\n"
+            echo -e "Under Construction\n"
+            sleep 1
+            echo -e "Press Enter to return to main menu..."
     ;;
         "Quit")
             echo "Exiting"
